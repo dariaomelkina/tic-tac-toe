338f15fd88c4503cc4f65e3abe3a49d192e5bead
Daria Omelkina <odasha2001@gmail.com>
2020-05-17 19:38:58 +0300

    minor changes;new tree

--- game_edition_task4.py
+++ game_edition_task4.py
@@ -2,15 +2,53 @@
 from board import Board
 from termcolor import cprint
 from tree_task_4 import Tree
+import copy
 
 class BoardSecondEdition(Board):
     def __init__(self):
         super().__init__()
         self.tree = None
+        self._last_symbol = None
 
     def tree(self):
+        tree = Tree(self)
+        def recursion(board, tree):
+            possible = self.possible()
+            if len([i for i in possible.keys()]) < 2:
+                board1 = copy.deepcopy(board)
+                if self._last_symbol == 'X':
+                    board1.add_pos(possible[1][0], possible[1][1], 'O')
+                else:
+                    board1.add_pos(possible[1][0], possible[1][1], 'X')
+                tree.add_child(board1)
+            else:
+                if self._last_symbol == "O":
+                    symbol = "X"
+                else:
+                    symbol = "O"
+                for i in possible.keys():
+                    board1 = copy.deepcopy(board)
+                    board1.add_pos(possible[i][0], possible[i][1], symbol)
+                    tree.add_child(board1)
+                for i in tree.children:
+                    recursion(i.key, i)
+        recursion(self, tree)
+        self.tree = tree
 
     def computer_move(self):
+        lst = self.tree.childen
+        points = []
+        for i in lst
+
+    def cut_tree(self):
+        for i in self.tree.children:
+            if i.key == self:
+                self.tree = i
+
+    def __eq__(self, other):
+        if self.positions == other.positions:
+            return True
+        return False
 
 
 def main():
@@ -37,4 +75,4 @@ if __name__ == '__main__':
     elif end == 'O':
         cprint('That time computer won.', 'yellow')
     else:
-        cprint('Draw! No has won, no one has lost.', 'magenta')
+        cprint('Draw! No one has won, no one has lost.', 'magenta')
--- tree_task_4.py
+++ tree_task_4.py
@@ -1,3 +1,18 @@
 class Tree:
     def __init__(self, root):
         self.key = root
+        self.children = []
+
+    def add_child(self, item):
+        self.children.append(Tree(item))
+
+    def leaves(self):
+        lst = []
+        def recursion(tree, lst):
+            if not tree.children:
+                lst.append(tree)
+            else:
+                for i in tree.children:
+                    recursion(i, lst)
+        recursion(self, lst)
+        return lst
